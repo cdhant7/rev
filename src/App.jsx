8ab11d0a374b94209503151f7aafa68db25bb6dd
SIDHANT <acharyasid1@gmail.com>
2023-11-23 21:33:35 +0545

    hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
+import { useState ,useEffect } from "react";
 
-const Person = (props) => {
-  return(
-    <>
-    <h1>Name:{props.name}</h1>
-    <h2>Last Name:{props.lastname}</h2>
-    <h3>Age:{props.age}</h3>
-    </>
-  )
-}
+// const Person = (props) => {
+//   return(
+//     <>
+//     <h1>Name:{props.name}</h1>
+//     <h2>Last Name:{props.lastname}</h2>
+//     <h3>Age:{props.age}</h3>
+//     </>
+//   )
+// }
 
-function App() {
+const App = () => {
+  const[counter,setCounter]=useState(0);
 
+  useEffect(() => {
+    setCounter(100);
+  },[])
   return (
   <div>
-    <Person name={'sid'} lastname={'acharya'} age={'23'}/>
-     <Person/>
-     <Person/>
-     <Person/>
+   <button onClick={() => setCounter((prevCount) => prevCount-1 ) } >-</button>
+   <h1>{counter}</h1>
+   <button onClick={() => setCounter((prevCount) => prevCount+1 ) } >+</button>
    </div>
   )
 }
